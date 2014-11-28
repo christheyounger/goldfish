c2033275509648978ba2902cedb0e4cf9bdca993
Chris Young <chris@chris-young.com>
2014-11-28 15:04:09 +1100

    Add a middleman template for goldfish. Include js and css to make it work nicely

--- src/Darkbluesun/GoldfishBundle/Controller/ProjectController.php
+++ src/Darkbluesun/GoldfishBundle/Controller/ProjectController.php
@@ -165,7 +165,7 @@ class ProjectController extends Controller
             'method' => 'PUT',
         ));
 
-        $form->add('submit', 'submit', array('label' => 'Update'));
+        //$form->add('submit', 'submit', array('label' => 'Update'));
 
         return $form;
     }
