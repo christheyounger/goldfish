fb65d2a48e7444b05366a4ad373faaa87bb86781
Chris Young <chris@chris-young.com>
2014-11-28 16:45:18 +1100

    add Task object

---
+++ src/Darkbluesun/GoldfishBundle/Entity/Task.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Darkbluesun\GoldfishBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Task
+ */
+class Task
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var \DateTime
+     */
+    private $due;
+
+    /**
+     * @var string
+     */
+    private $description;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Task
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set due
+     *
+     * @param \DateTime $due
+     * @return Task
+     */
+    public function setDue($due)
+    {
+        $this->due = $due;
+
+        return $this;
+    }
+
+    /**
+     * Get due
+     *
+     * @return \DateTime 
+     */
+    public function getDue()
+    {
+        return $this->due;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Task
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
