450de69e56126f1b9774e3f9b724b109a7f187e5
Chris Young <chris@chris-young.com>
2014-11-28 17:19:25 +1100

    Use annotations instead of yml

--- src/Darkbluesun/GoldfishBundle/Entity/Task.php
+++ src/Darkbluesun/GoldfishBundle/Entity/Task.php
@@ -6,26 +6,39 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * Task
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Darkbluesun\GoldfishBundle\Entity\TaskRepository")
  */
 class Task
 {
     /**
      * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
      */
     private $id;
 
     /**
      * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
      */
     private $name;
 
     /**
      * @var \DateTime
+     *
+     * @ORM\Column(name="due", type="datetime")
      */
     private $due;
 
     /**
      * @var string
+     *
+     * @ORM\Column(name="description", type="string")
      */
     private $description;
 
