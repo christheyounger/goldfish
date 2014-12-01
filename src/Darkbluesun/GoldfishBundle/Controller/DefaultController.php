761bd3d99ba92adfbccade487e4ce9319875590f
Chris Young <chris@chris-young.com>
2014-12-02 10:12:28 +1100

    Add login form

--- src/Darkbluesun/GoldfishBundle/Controller/DefaultController.php
+++ src/Darkbluesun/GoldfishBundle/Controller/DefaultController.php
@@ -3,11 +3,42 @@
 namespace Darkbluesun\GoldfishBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\SecurityContextInterface;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('DarkbluesunGoldfishBundle:Default:index.html.twig', array('name' => 'borris'));
+    }
+
+    public function loginAction(Request $request)
+    {
+    	$session = $request->getSession();
+
+        // get the login error if there is one
+        if ($request->attributes->has(SecurityContextInterface::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(
+                SecurityContextInterface::AUTHENTICATION_ERROR
+            );
+        } elseif (null !== $session && $session->has(SecurityContextInterface::AUTHENTICATION_ERROR)) {
+            $error = $session->get(SecurityContextInterface::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContextInterface::AUTHENTICATION_ERROR);
+        } else {
+            $error = '';
+        }
+
+        // last username entered by the user
+        $lastUsername = (null === $session) ? '' : $session->get(SecurityContextInterface::LAST_USERNAME);
+
+        return $this->render(
+            'DarkbluesunGoldfishBundle:Default:login.html.twig',
+            array(
+                // last username entered by the user
+                'last_username' => $lastUsername,
+                'error'         => $error,
+            )
+        );
     }
 }
